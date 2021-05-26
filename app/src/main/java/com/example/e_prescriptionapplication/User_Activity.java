e445c8910283bc77b66698e75ae1c1065f84c26c
Shamrock66 <84872141+Shamrock66@users.noreply.github.com>
2021-05-26 14:00:41 -0500

    Initial Commit

---
+++ app/src/main/java/com/example/e_prescriptionapplication/User_Activity.java
@@ -0,0 +1,44 @@
+package com.example.e_prescriptionapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class User_Activity extends AppCompatActivity {
+
+    Button mPharmcistBtn, mPhysicianBtn, mPatientBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user);
+
+        mPharmcistBtn= findViewById(R.id.pharmacistBtn);
+        mPhysicianBtn= findViewById(R.id.physicianBtn);
+        mPatientBtn= findViewById(R.id.patientBtn);
+
+        mPharmcistBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(),PharmacistRegister.class));
+            }
+        });
+
+        mPhysicianBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(),PhysicianRegister.class));
+            }
+        });
+
+        mPatientBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(),PatientRegister.class));
+            }
+        });
+    }
+}
